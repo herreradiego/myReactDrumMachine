22c02c9454df6470df8d1b016b9e5ac99ac11433
herreradiego <m.diego.herrera@gmail.com>
2019-10-08 22:59:15 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import buttons from './buttons'
 import './App.css';
 
+import KeyA from './sounds/clap.wav'
+import KeyS from './sounds/hihat.wav'
+import KeyD from './sounds/kick.wav'
+import KeyF from './sounds/openhat.wav'
+import KeyG from './sounds/boom.wav'
+import KeyH from './sounds/ride.wav'
+import KeyI from './sounds/snare.wav'
+import KeyJ from './sounds/tom.wav'
+import KeyK from './sounds/tink.wav'
+
+
+
+
 function App() {
+
+
+
+const sounds = {
+  65:KeyA,
+  83:KeyS,
+  68:KeyF,
+  70:KeyD,
+  71:KeyG,
+  72:KeyH,
+  74:KeyI,
+  75:KeyJ,
+  76:KeyK
+}
+
+useEffect(()=>{
+  document.addEventListener('keydown',(event)=>{
+    
+    
+    console.log("aprestaste: ",event.keyCode)
+    let theKey =event.keyCode ;
+    let theSound = new Audio(sounds[theKey])
+    theSound.play()
+  },false)
+})
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      {buttons.map(button=>{
+        return(
+          <button className="key-btn" name={button.buttonName}>{button.buttonText}</button>
+        )
+      })}
+
+
     </div>
   );
 }
---
+++ src/buttons.js
@@ -0,0 +1,42 @@
+const buttons = [
+    {
+    button:"A",
+    buttonText:"A - Clap"
+  
+    },
+    {
+      buttonName:"S",
+      buttonText:"S - Hihat"
+    },
+    {
+      buttonName:"D",
+      buttonText:"D - Kick"
+    },
+    {
+      buttonName:"F",
+      buttonText:"F - Open Hat"
+    },
+    {
+      buttonName:"G",
+      buttonText:"G - Boom"
+    },
+    {
+      buttonName:"H",
+      buttonText:"H - Ride"
+    },
+    {
+      buttonName:"J",
+      buttonText:"J - Snare"
+    },
+    {
+      buttonName:"K",
+      buttonText:"K - Tom"
+    },
+    {
+      buttonName:"L",
+      buttonText:"L - Tink"
+    }
+  ]
+
+
+  export default buttons
