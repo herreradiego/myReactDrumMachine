e7ca36efa579812d46dcbb63549075d9875e477a
herreradiego <m.diego.herrera@gmail.com>
2019-10-09 22:44:39 -0300

    Updates

---
+++ precache-manifest.44ee1fb214dd63502b47c4490270cb6b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8c5e7e4af0e05bd81c9875018d6765",
+    "url": "/myReactDrumMachine/index.html"
+  },
+  {
+    "revision": "7de6c6484804824e3192",
+    "url": "/myReactDrumMachine/static/css/main.4bcd1516.chunk.css"
+  },
+  {
+    "revision": "6e8b669ea521cfb1fe0f",
+    "url": "/myReactDrumMachine/static/js/2.42cb140b.chunk.js"
+  },
+  {
+    "revision": "7de6c6484804824e3192",
+    "url": "/myReactDrumMachine/static/js/main.1d2437ae.chunk.js"
+  },
+  {
+    "revision": "474d84339da0f48daab8",
+    "url": "/myReactDrumMachine/static/js/runtime-main.4413319a.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/myReactDrumMachine/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/myReactDrumMachine/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/myReactDrumMachine/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/myReactDrumMachine/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/myReactDrumMachine/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/myReactDrumMachine/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/myReactDrumMachine/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/myReactDrumMachine/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/myReactDrumMachine/static/media/tom.572ed39d.wav"
+  }
+]);
